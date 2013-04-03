f372355f9bf777c9e5332eef97c763ca59fdbe88
Cody Spriggs <codyspriggs@me.com>
2013-04-02 22:46:57 -0500

    Correctly storing geocoded addresses for new events

--- js/site.js
+++ js/site.js
@@ -1,7 +1,72 @@
 $(document).ready(function(){
+  
+  var geocoder;
+  var address;
+  var map;
 
-  //object for itinerary data
-  newItinerary = {};
+//********** Google Maps API *******************
+
+  //object to store map properties
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var myLocation = new google.maps.LatLng(40.750556, -73.993611);
+    var mapOptions = {
+      center: myLocation,
+      zoom:13,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+
+    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    // google.maps.event.trigger(map, 'resize');
+    // get current center of map to be resized 
+    var currentCenter = map.getCenter();
+
+    $('a[href="#mapContainerTab"]').on('shown', function(e){
+        google.maps.event.trigger(map, 'resize');
+        // re-Center the map due to hidden tab div
+        map.setCenter(currentCenter);
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLocation,
+      title:'Click to zoom',
+      map: map
+    });
+
+    marker.setMap(map);
+
+    // Zoom to 17 when clicking on marker
+    google.maps.event.addListener(marker,'click',function() {
+      map.setZoom(17);
+      map.setCenter(marker.getPosition());
+    });
+
+  };//end initialize
+
+
+  function getGeoCode(callback) {
+    var address = $('#inputAddress').val();
+    geocoder.geocode( {'address': address}, function(results,status){
+      if (status == google.maps.GeocoderStatus.OK) {
+        callback(results[0].geometry.location.lat(), results[0].geometry.location.lng() );
+        // alert("latitude : " + results[0].geometry.location.lat() );
+        // alert("longitude : " + results[0].geometry.location.lng() );
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+        });
+        // console.log(results[0].geometry.location);
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });//end geocoder
+  }//end getGeoCode
+
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
 
 //********** Date Picker Plugin ***********************
 
@@ -96,6 +161,7 @@ $(document).ready(function(){
       dinnerEnd[0] += 12
     }
 
+    getGeoCode(function (lat, lng){
 
         //send data to Backlift
          $.ajax({
@@ -129,11 +195,9 @@ $(document).ready(function(){
                 }
 
               ],
+              geocode: [lat,lng],
               venueName: $('#inputVenueName').val(),
               venueAddress: $('#inputAddress').val(),
-              venueCity: $('#inputVenueCity').val(),
-              venueState: $('#inputVenueState').val(),
-              venueZIP: $('#inputZIP').val(),
               hotelName: $('#inputHotelName').val(),
               hotelAddress: $('#inputHotelAddress').val()
           }),
@@ -143,7 +207,8 @@ $(document).ready(function(){
             $('#calendar').fullCalendar('refetchEvents');
           }
         });//end POST
-    
+
+    });//end getGeoCode
 
   });//end click
 
@@ -163,46 +228,5 @@ $(document).ready(function(){
     eventClick: viewShow,
   });//end fullCalendar
 
-//********** Google Maps API *******************
-  
-
-  //object to store map properties
-  function initialize() {
-    var myLatlng = new google.maps.LatLng(40.7506573,-73.9934726);
-    var mapOptions = {
-      center: myLatlng,
-      zoom:13,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-
-    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-    // google.maps.event.trigger(map, 'resize');
-    // get current center of map to be resized 
-    var currentCenter = map.getCenter();
-
-    $('a[href="#mapContainerTab"]').on('shown', function(e){
-        google.maps.event.trigger(map, 'resize');
-        // re-Center the map due to hidden tab div
-        map.setCenter(currentCenter);
-    });
-
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      title:'Click to zoom',
-      map: map
-    });
-
-    marker.setMap(map);
-
-    // Zoom to 17 when clicking on marker
-    google.maps.event.addListener(marker,'click',function() {
-      map.setZoom(17);
-      map.setCenter(marker.getPosition());
-    });
-
-  };//end initialize
-
-  google.maps.event.addDomListener(window, 'load', initialize);
 
 });//end doc ready
